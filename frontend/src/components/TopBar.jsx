fd434d8f408624897125efe834703a7f1543ebba
LorelieC <mlcanete1@tip.edu.ph>
2025-07-02 09:25:15 +0800

    Final Nav bar

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -6,16 +6,21 @@ import ShoppingBag from '../assets/Shopping_bag.png';
 import AccountCircle from '../assets/account_circle.png';
 
 const TopBar = () => (
-  <header className="navbar">
+  <div className="navbar">
     <div className="nav-left">
-      <img src={LogoNav} alt="PS Logo Leaf" className="logo-img" />
+      <img
+         src={LogoNav}
+          alt="Logo"
+          className="logo-img"
+          style={{ maxHeight: '130px', marginTop: '-57px',marginLeft: '-55px' }}
+      />
     </div>
     <div className="nav-right">
       <input type="text" placeholder="Search..." className="search-box" />
       <img src={ShoppingBag} alt="Shopping Bag Icon" className="nav-icon" />
       <img src={AccountCircle} alt="Account Circle Icon" className="nav-icon" />
     </div>
-  </header>
+  </div>
 );
 
 export default TopBar;
