9b40d82e30647c4cc9b891b76593a36c1a23940e
akinitos <vonarcibal@gmail.com>
2025-07-14 21:20:38 +0800

    f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
- base: "/pikosen/",
-  build: {
-    outDir: "docs", 
-  },
-})
+  base: '/pikosen/',
+  build: { outDir: 'docs' },
+  server: { historyApiFallback: true, },
+});
