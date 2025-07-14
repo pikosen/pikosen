c1c0516731dfbd403cf537d2f1941e83474797fa
akinitos <vonarcibal@gmail.com>
2025-07-14 16:24:47 +0800

    ops nalimot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -85,9 +85,9 @@ function App() {
           <Route
             path="/dashboard/"
             element={
-            /*<ProtectedRoute>*/
+            <ProtectedRoute>
               <Dashboard />
-            /*</ProtectedRoute>*/
+            </ProtectedRoute>
             }
           />
           <Route 
