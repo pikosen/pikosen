6aa7e8dc340652d0357195c4da9b294c726fbd8f
akinitos <vonarcibal@gmail.com>
2025-07-14 21:55:40 +0800

    f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
         />
         <Route
           path="/register"
-          element={<RegisterAndLogout />}
+          element={<Register />}
         />
         <Route path="*"
           element={<NotFound />} 
