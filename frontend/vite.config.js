a34af3e19df42c0b6fd6e61e9006d2e02395bff0
LorelieC <mlcanete1@tip.edu.ph>
2025-07-15 02:03:26 +0800

    another changes for vercel after failed deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/pikosen/',
-  build: { outDir: 'docs' },
+  build: { outDir: 'dist' },
   server: { historyApiFallback: true, },
-  base: process.env.VITE_BASE_PATH || "/pikosen"
+  base: process.env.VITE_BASE_PATH || '/pikosen/',
 });
