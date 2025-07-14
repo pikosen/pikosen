aa0d14dc74c0f8317e701890cb6ad48411824dd0
akinitos <vonarcibal@gmail.com>
2025-07-14 19:58:22 +0800

    tanggal ka na jekyll lumayas ka

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs', 
-  },
+  build: { outDir: 'docs' },
   server: {
     historyApiFallback: true, 
   },
