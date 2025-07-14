573f8a1b3c8d9b6022e068a32e4257ba66bf2264
akinitos <vonarcibal@gmail.com>
2025-07-14 20:14:53 +0800

    .nojekylling off

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: { outDir: 'docs' },
-  server: {
-    historyApiFallback: true, 
-  },
+  server: { historyApiFallback: true, },
 });
