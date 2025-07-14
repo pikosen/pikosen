408ddb2dc00ee0a9147106fbc9a0e99e1f0222dd
LorelieC <mlcanete1@tip.edu.ph>
2025-07-15 02:23:08 +0800

    add vite config changes for vercel on main branch for testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pikosen/',
-  build: { outDir: 'docs' },
+  base: process.env.VITE_BASE_PATH || '/pikosen/',
+  build: { outDir: 'dist' },
   server: { historyApiFallback: true, },
 });
