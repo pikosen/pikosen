fecce0aa29a13bfe6730a2c5ed15caf88f2a8cc4
eugeniuss7 <eugenevillegas47@gmail.com>
2025-07-14 18:50:29 +0800

    tried fixing settings

--- Pikosen/Pikosen/settings.py
+++ Pikosen/Pikosen/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
+
+SECRET_KEY = 'django-insecure-dj%+gs1_$rhf0@wczp8b1$!75knv#5ux())su^f&y6sf8rkm0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
-
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
