2b3da4ca69feaa83d745621f6248fca2115a9748
eugeniuss7 <eugenevillegas47@gmail.com>
2025-07-15 10:53:28 +0800

    Fix create account

--- frontend/src/components/AccountForm.jsx
+++ frontend/src/components/AccountForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from "react-router-dom"
 function AccountForm({route}) {
 
     // --- STATE VARIABLES ---
-    // try
+    // tryers
     // Form state variables
     const [user, setUser] = useState(null)
     const [name, setName] = useState("")
