ff24f879326fb9a4d2f582dfab8e594d5a86d841
akinitos <vonarcibal@gmail.com>
2025-07-15 20:19:02 +0800

    agoi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Navigate to="/login/" replace />} />
+        <Route path="/" element={<Navigate to="/login/" replace />} />
 
         <Route path="/login/" element={<Login />} />
         <Route path="/logout/" element={<Logout />} />
